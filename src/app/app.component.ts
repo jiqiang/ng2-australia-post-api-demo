79b408df94cffb2fadfaeb887f5272ccb1542a36
Glenn Ji <qiang.ji@yahoo.com.au>
2017-03-05 23:13:21 +1100

    intial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PacService } from 'ng2-australia-post-api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  countries: any[];
+
+  constructor(private pacService: PacService) {
+    this.pacService.getCountries()
+      .then(countries => this.countries = countries);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { AustraliaPostAPIModule } from 'ng2-australia-post-api';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    AustraliaPostAPIModule.forRoot({auth_key: 'e6f7e18e-c319-46d2-84b3-4f8177f0d8f2'})
   ],
   providers: [],
   bootstrap: [AppComponent]
